522b188f7c4d0e91d3351fb0ebd41af637711bfa
Suman Naskar <sumannas007@gmail.com>
2025-12-10 13:25:38 +0530

    another fix in database

--- app/db/database.py
+++ app/db/database.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv() 
 
-DATABASE_URL = str(os.getenv("DATABASE_UR"))
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(url=DATABASE_URL, echo=True)
 
